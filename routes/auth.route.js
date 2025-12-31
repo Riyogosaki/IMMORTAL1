464bc7ca6543035c7fe80bcd20635abb7d850895
Ankush Kumar <ankushkumae467@gmail.com>
2025-12-31 13:18:13 +0530

    go

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { checkAuth, login, logout, signup, updateProfile,verifyEmailOtp } from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 router.post("/signup", signup);          // Create user & send OTP
-router.post("/verify-otp", verifyEmailOtp); // Verify OTP and mark user verified
 router.post("/login", login);            // Login only allowed after verification
 
 router.post("/logout", logout);
